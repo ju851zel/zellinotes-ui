b248ea7b4f8c3fd9b92d79c8054dc3b9c728786d
julian <ju851zel@htwg-konstanz.de>
2020-08-17 12:22:33 +0200

    added recipe module

---
+++ src/app/recipe/recipe.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { RecipeOverviewComponent } from './recipe-overview/recipe-overview.component';
+
+
+@NgModule({
+  declarations: [RecipeOverviewComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RecipeModule {
+}
