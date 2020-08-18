48ad46c98b1fbc1a2a7d14e6a383044cf0d17be9
julian <ju851zel@htwg-konstanz.de>
2020-08-18 16:22:53 +0200

    updated recipe module

--- src/app-module/modules/recipes/recipe.module.ts
+++ src/app-module/modules/recipes/recipe.module.ts
@@ -1,16 +1,28 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RecipeOverviewComponent} from './recipe-overview-component/recipe-overview.component';
+import {RecipeOverviewComponent} from './recipe-overview/recipe-overview-component/recipe-overview.component';
 import {IconsModule} from '../icons/icons.module';
+import { UploadRecipesModalComponent } from './upload-recipes-modal/upload-recipes-modal.component';
+import { RecipeCardsComponent } from './recipe-overview/recipe-cards-component/recipe-cards.component';
+import {NgxMasonryModule} from 'ngx-masonry';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SingleRecipeComponent } from './single-recipe/single-recipe-component/single-recipe.component';
+import { RecipeUploadCropImageComponent } from './single-recipe/recipe-upload-crop-image/recipe-upload-crop-image.component';
 
 
 @NgModule({
   declarations: [
-    RecipeOverviewComponent
+    RecipeOverviewComponent,
+    UploadRecipesModalComponent,
+    RecipeCardsComponent,
+    SingleRecipeComponent,
+    RecipeUploadCropImageComponent
   ],
   imports: [
     CommonModule,
-    IconsModule
+    IconsModule,
+    NgxMasonryModule,
+    BrowserAnimationsModule
   ]
 })
 export class RecipeModule {
