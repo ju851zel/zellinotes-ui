06cd20f4389186f34548c59fed73f387a0db49dc
julian <ju851zel@htwg-konstanz.de>
2020-08-18 16:22:35 +0200

    updated routes

--- src/app-module/modules/app-routing/app-routing.module.ts
+++ src/app-module/modules/app-routing/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {RecipeOverviewComponent} from '../recipes/recipe-overview-component/recipe-overview.component';
+import {RecipeOverviewComponent} from '../recipes/recipe-overview/recipe-overview-component/recipe-overview.component';
 import {HomeComponent} from '../home/home-component/home.component';
+import {SingleRecipeComponent} from '../recipes/single-recipe/single-recipe-component/single-recipe.component';
 
 
 const routes: Routes = [
-  { path: 'recipes', component: RecipeOverviewComponent },
-  { path: 'home', component: HomeComponent },
+  { path: ':recipes/:recipeId', component: SingleRecipeComponent },
+  { path: ':recipes', component: RecipeOverviewComponent },
+  { path: ':home', component: HomeComponent },
 ];
 
 
