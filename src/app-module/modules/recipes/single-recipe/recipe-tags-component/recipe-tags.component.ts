f2113d647e5729e724ec5797d94c49b31087f36e
julian <ju851zel@htwg-konstanz.de>
2020-08-20 13:08:06 +0200

    changed recipe tags component

--- src/app-module/modules/recipes/single-recipe/recipe-tags-component/recipe-tags.component.ts
+++ src/app-module/modules/recipes/single-recipe/recipe-tags-component/recipe-tags.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-tags',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-tags.component.css']
 })
 export class RecipeTagsComponent implements OnInit {
+
+  @Input()
+  private recipeId: string;
 
   constructor() { }
 
