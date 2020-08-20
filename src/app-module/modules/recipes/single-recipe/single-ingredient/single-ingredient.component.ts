0a7cd081e6fc11d516f68e60c3c37816a898e655
julian <ju851zel@htwg-konstanz.de>
2020-08-20 13:56:50 +0200

    added single ingredient

---
+++ src/app-module/modules/recipes/single-recipe/single-ingredient/single-ingredient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-ingredient',
+  templateUrl: './single-ingredient.component.html',
+  styleUrls: ['./single-ingredient.component.css']
+})
+export class SingleIngredientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
