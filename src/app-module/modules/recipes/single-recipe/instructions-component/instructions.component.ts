064a70b6c6365b485396afe9da55182f0b15beb2
julian <ju851zel@htwg-konstanz.de>
2020-08-20 13:08:22 +0200

    updated instructions component

--- src/app-module/modules/recipes/single-recipe/instructions-component/instructions.component.ts
+++ src/app-module/modules/recipes/single-recipe/instructions-component/instructions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-instructions',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstructionsComponent implements OnInit {
 
-  constructor() { }
+
+  @Input()
+  private recipeId: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
