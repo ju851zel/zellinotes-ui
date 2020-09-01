4be990428f001a4af05588d24f972873a80c255c
julian <ju851zel@htwg-konstanz.de>
2020-09-01 23:12:02 +0200

    updated top buttons component

--- src/app-module/modules/recipes/single-recipe/top-buttons/top-buttons.component.ts
+++ src/app-module/modules/recipes/single-recipe/top-buttons/top-buttons.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-top-buttons',
   templateUrl: './top-buttons.component.html',
   styleUrls: ['./top-buttons.component.css']
 })
-export class TopButtonsComponent implements OnInit {
+export class TopButtonsComponent {
 
-  constructor() { }
+  @Input() editMode: boolean;
 
-  ngOnInit(): void {
+  @Output() navigateToRecipes = new EventEmitter<any>();
+  @Output() editableToggled = new EventEmitter<any>();
+  @Output() deleteRecipe = new EventEmitter<any>();
+  @Output() downloadRecipe = new EventEmitter<any>();
+  @Output() duplicateRecipe = new EventEmitter<any>();
+
+  onNavigateToRecipes(): void {
+    this.navigateToRecipes.emit(true);
+  }
+
+  onToggleEditable(): void {
+    this.editableToggled.emit(true);
+  }
+
+  onDeleteRecipe(): void {
+    this.deleteRecipe.emit(true);
+  }
+
+  onDownloadRecipe(): void {
+    this.downloadRecipe.emit(true);
+  }
+
+  onDuplicateRecipe(): void {
+    this.duplicateRecipe.emit(true);
   }
 
 }
