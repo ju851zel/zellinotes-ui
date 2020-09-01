f1801b8371fb52c17f51d17424fdcddcb60b5575
julian <ju851zel@htwg-konstanz.de>
2020-09-01 17:23:47 +0200

    removed add instruction button with automated mechanism

--- src/app-module/modules/recipes/single-recipe/instructions-component/instructions.component.ts
+++ src/app-module/modules/recipes/single-recipe/instructions-component/instructions.component.ts
@@ -1,16 +1,16 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-instructions',
   templateUrl: './instructions.component.html',
   styleUrls: ['./instructions.component.css']
 })
-export class InstructionsComponent implements OnInit {
+export class InstructionsComponent implements OnInit, OnChanges {
 
   @Input()
   private defaultInstructions: Array<string>;
   @Input()
-  editable: boolean;
+  private editable: boolean;
 
   @Output()
   instructionChanged = new EventEmitter<Array<string>>();
@@ -22,31 +22,54 @@ export class InstructionsComponent implements OnInit {
     this.instructions = Object.assign([], this.defaultInstructions);
   }
 
+  editMode(): boolean {
+    return this.editable;
+  }
+
+  viewMode(): boolean {
+    return !this.editMode();
+  }
+
   trackByInstructions(index: number, item: any): number {
     return index;
   }
 
-  addInstruction(event: Event): void {
-    event.stopPropagation();
-    this.instructions.push('');
-    this.instructionsUpdated();
-    this.showCollapse();
-  }
-
-  showCollapse(): void {
-    this.collapsed = false;
-  }
-
-  instructionsUpdated(): void {
-    this.instructionChanged.emit(this.instructions);
+  private lastInstructionNotEmpty(): boolean {
+    return this.instructions?.length === 0 || this.instructions[this.instructions.length - 1].length !== 0;
   }
 
   deleteInstruction(index: number): void {
     this.instructions.splice(index, 1);
-    this.instructionsUpdated();
+    this.notifyInstructionsUpdated();
   }
 
   toggleCollapse(): void {
     this.collapsed = !this.collapsed;
+  }
+
+  notifyInstructionsUpdated(): void {
+    const finalInstructions = this.instructions.filter(inst => inst.length > 0);
+    this.instructionChanged.emit(finalInstructions);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.editMode() && this.lastInstructionNotEmpty()) {
+      this.instructions.push('');
+    } else if (this.viewMode() && this.lastInstructionIsEmpty()) {
+      this.instructions.pop();
+    }
+  }
+
+
+  private lastInstructionIsEmpty(): boolean {
+    return !!this.instructions && this.instructions[this.instructions.length - 1].length === 0;
+  }
+
+  noInstructions(): boolean {
+    return this.instructions.length === 0;
+  }
+
+  minOneInstruction(): boolean {
+    return this.instructions.length > 0;
   }
 }
