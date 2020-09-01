7722a39cbb75fbca1ac4eb9cd161328e8e5902a0
julian <ju851zel@htwg-konstanz.de>
2020-09-01 12:49:16 +0200

    updated single ingredient component

--- src/app-module/modules/recipes/single-recipe/single-ingredient/single-ingredient.component.spec.ts
+++ src/app-module/modules/recipes/single-recipe/single-ingredient/single-ingredient.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SingleIngredientComponent } from './single-ingredient.component';
+import {SingleIngredientComponent} from './single-ingredient.component';
 
 describe('SingleIngredientComponent', () => {
   let component: SingleIngredientComponent;
@@ -8,9 +8,9 @@ describe('SingleIngredientComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SingleIngredientComponent ]
+      declarations: [SingleIngredientComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app-module/modules/recipes/single-recipe/single-ingredient/single-ingredient.component.ts
+++ src/app-module/modules/recipes/single-recipe/single-ingredient/single-ingredient.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Ingredient, MeasurementUnit} from '../../../../model/recipe';
+
+class MutableIngredient {
+  constructor(public id: number,
+              public amount: number,
+              public title: string,
+              public unit: string) {
+  }
+
+  toImmutable(): Ingredient {
+    return new Ingredient(
+      this.id,
+      this.amount,
+      this.title,
+      MeasurementUnit.allUnits.find(unit => unit.title === this.unit),
+    );
+  }
+}
+
 
 @Component({
   selector: 'app-single-ingredient',
   templateUrl: './single-ingredient.component.html',
-  styleUrls: ['./single-ingredient.component.css']
 })
 export class SingleIngredientComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  defaultIngredient: Ingredient;
 
-  ngOnInit(): void {
+  @Output()
+  ingredientChanged = new EventEmitter<Ingredient>();
+
+  @Output()
+  deleteIngredient = new EventEmitter<boolean>();
+
+  ingredient: MutableIngredient;
+  allMeasurementUnits: Array<MeasurementUnit> = MeasurementUnit.allUnits;
+
+  constructor() {
   }
 
+  ngOnInit(): void {
+    const ing = this.defaultIngredient.copy();
+    this.ingredient = new MutableIngredient(ing.id, ing.amount, ing.title, ing.measurementUnit?.title);
+  }
+
+  ingredientUpdated(): void {
+    console.log(this.ingredient);
+    this.ingredientChanged.emit(this.ingredient.toImmutable());
+  }
+
+  onDeleteIngredientClicked(): void {
+    this.deleteIngredient.emit(true);
+  }
 }
