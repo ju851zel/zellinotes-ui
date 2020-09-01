7eea0654f97357965ab2999dd897f9559d7673db
julian <ju851zel@htwg-konstanz.de>
2020-09-01 12:49:55 +0200

    updated recipe tags

--- src/app-module/modules/recipes/single-recipe/recipe-tags-component/recipe-tags.component.ts
+++ src/app-module/modules/recipes/single-recipe/recipe-tags-component/recipe-tags.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {RecipeService} from '../../../../services/recipe-service/recipe.service';
+import {Recipe} from '../../../../model/recipe';
 
 @Component({
   selector: 'app-recipe-tags',
@@ -8,11 +10,25 @@ import {Component, Input, OnInit} from '@angular/core';
 export class RecipeTagsComponent implements OnInit {
 
   @Input()
-  private recipeId: string;
+  public recipe: Recipe;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private recipeService: RecipeService) {
   }
 
+  ngOnInit(): void {
+    console.log(this.recipe);
+  }
+
+  createTagInputValue(): string {
+    return [...this.recipe.tags].join(' ');
+  }
+
+  onTagUpdate(value: string): void {
+    const tags = value.split(' ').filter(tag => tag !== '');
+    this.updateRecipe(this.recipe.copyButTags(new Set(tags)));
+  }
+
+  updateRecipe(recipe: Recipe): void {
+    this.recipeService.replaceRecipe(this.recipe.id, recipe);
+  }
 }
